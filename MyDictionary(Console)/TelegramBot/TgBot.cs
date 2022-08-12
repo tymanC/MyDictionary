81e16c35402dc444a64582d4eade2019ba2d9b11
tymanC <mularcukanastasia@gmail.com>
2022-08-12 20:13:08 +0300

    Add tg bot info without message handler

---
+++ MyDictionary(Console)/TelegramBot/TgBot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using MyDictionary_Console_.Constants;
+using Telegram.Bot.Exceptions;
+
+namespace MyDictionary_Console_.TelegramBot
+{
+    internal class TgBot
+    {
+        TelegramBotClient botClient = new TelegramBotClient(Constant.BotToken);
+        CancellationToken cancellationToken = new CancellationToken();
+        ReceiverOptions receiverOptions = new ReceiverOptions { AllowedUpdates = { } };
+
+        //start bot
+        public async Task Start()
+        {
+            botClient.StartReceiving(HandlerUpdateAsync, HandlerError, receiverOptions, cancellationToken);
+            var botMe = await botClient.GetMeAsync();
+            Console.WriteLine($"Бот {botMe.Username} почав працювати");
+            Console.ReadKey();
+        }
+
+        private Task HandlerError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Помилка в телеграм бот АПІ:\n{apiRequestException.ErrorCode}" +
+                $"{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandlerUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message && update?.Message?.Text != null)
+            {
+                //запуск методу, що обробляє текст
+                await HandlerMessageAsync(botClient, update.Message);
+            }
+        }
+        private async Task HandlerMessageAsync(ITelegramBotClient botClient, Message message)
+        {
+
+        }
+
+    }
+}
