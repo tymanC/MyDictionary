7d9fab2006330df71403767bf66986d30c87199f
tymanC <mularcukanastasia@gmail.com>
2022-08-13 13:39:03 +0300

    upd

--- MyDictionary(Console)/Program.cs
+++ MyDictionary(Console)/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyDictionary_Console_.LanguageScoringApi;
+using MyDictionary_Console_.TelegramBot;
 
 namespace MyDictionary_Console_
 {
@@ -11,9 +12,10 @@ namespace MyDictionary_Console_
     {
         static void Main(string[] args)
         {
-            LangScoringClient client = new LangScoringClient();
-            string word = "coincide";
-            Console.WriteLine(client.GetWordScore(word).Result.Ten_degree);
+            TgBot bot = new TgBot();
+            bot.Start();
+            Console.ReadKey();
+        
         }
     }
 }
